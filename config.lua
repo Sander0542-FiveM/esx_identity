32ea346db547f77eb025e6ffd36512427e551135
Tommy Hosewol <t.hosewol@gmail.com>
2020-10-01 19:47:25 +0200

    Wijziging lengtes

--- config.lua
+++ config.lua
@@ -21,8 +21,8 @@ Config.UseDeferrals     = false -- EXPERIMENTAL Character Registration Method.
 
 -- These values are for the second input validation in server/main.lua
 Config.MaxNameLength    = 16
-Config.MinHeight        = 48
-Config.MaxHeight        = 96
+Config.MinHeight        = 100
+Config.MaxHeight        = 250
 Config.LowestYear       = 1900
 Config.HighestYear      = 2020
 
